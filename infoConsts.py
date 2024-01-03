314dab80ae0d1f97dc4a3dbc8ff4f154fd39fe71
MdrskJ <modorskiy.jakov@gmail.com>
2024-01-04 01:00:34 +0500

    Четвёртый коммит: 1. Изменено количество аргументов инормационного окна, теперь достаточно одного номера элемента 2. Внесена большая часть данных в информационное окно, но нужно переделать файл data в csv формат 3. Видоизменён файл info.ui теперь информация отделена островками 4. В файл infoConsts добавлен паттерн стиля островков с информацией

--- infoConsts.py
+++ infoConsts.py
@@ -1,2 +1,4 @@
 ui_path = 'info.ui'
 picture_dir_path = 'pictures_of_elements'
+
+island_style = "background-color: rgb(100, 100, 100); border-radius: 20px;"
--- widgets/elementInfo.py
+++ widgets/elementInfo.py
@@ -1,15 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QSizePolicy
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
+
 from infoConsts import *
+from data import arr_of_elements
 
 
 class InfoEl(QWidget):
-    def __init__(self, *args):
+    def __init__(self, num):
         super().__init__()
-        self.args = args
+        self.num = num
+        self.args = arr_of_elements[num]
         uic.loadUi(ui_path, self)
         self.loadInfo()
 
@@ -21,22 +24,50 @@ class InfoEl(QWidget):
             "border: 0px solid #094065;"
         )
 
-        self.line1_background.setStyleSheet(
-            "background-color: rgb(100, 100, 100);"
-            "border-radius: 20px;"
-        )
-        self.sym.setText(self.args[1])
-        self.name.setText(self.args[2])
-        pixmap = QPixmap(f'pictures_of_elements/{self.args[0]}.jpg')
+    # вносим данные в line1
+        self.line1_background.setStyleSheet(island_style)
+        self.sym.setText(self.args["symbol"])
+        self.name.setText(self.args["name"])
+        pixmap = QPixmap(f'{picture_dir_path}/{self.num}.jpg')
         pixmap = pixmap.scaled(150, 150)
         self.picture_of_element.setPixmap(pixmap)
 
+    # вносим данные в line2
+        self.line2_background.setStyleSheet(island_style)
+        self.ch_electron.setText(self.args["atomic"])
+        self.ch_proton.setText(self.args["atomic"])
+        self.ch_neutron.setText(self.cntNeutron())
 
+    # вносим данные в line3
+        self.line3_background.setStyleSheet(island_style)
+        self.ch_atomic_num.setText(self.args["atomic"])
+        self.ch_mass.setText(self.args["weight"])
+
+    # вносим данные в line4 и line5
+        self.line45_background.setStyleSheet(island_style)
+        self.text_electron_config.setText(self.args["expandedconfig"])
+        self.text_electron_string.setText(self.args["electronstring"])
+
+        if "valence" in self.args:
+            self.text_valence.setText("0")
+        else:
+            self.text_valence.setText(self.args["oxidation"])
+
+    # вносим данные в line6
+        self.line6_background.setStyleSheet(island_style)
+        self.date_discover.setText(self.args["discover"])
+        self.ch_melt.setText(self.args["melt"])
+        self.ch_boil.setText(self.args["boil"])
+        self.ch_density.setText(self.args["density"]["stp"])
+        self.ch_isotopes.setText(str(self.args["isotopes"]))
+
+    def cntNeutron(self):
+        return str(round(float(self.args["weight"])) - self.num)
 
 
 def main():
     app = QApplication(sys.argv)
-    ex = InfoEl(10, 'Ne', 'Неон', '20.1797', '66AAFF', 8, 2, 19)
+    ex = InfoEl(90)
     ex.show()
     sys.exit(app.exec())
 
